80ce233054c37abd06cdb0a4dcfadb941d0c0a2b
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-04-04 10:54:59 -0300

    feat(Contar quantos números divisíveis por 3 existem entre 1 e N (N = 50))

--- exercicio_08.js
+++ exercicio_08.js
@@ -1,7 +1,7 @@
-let N = 50
+let N = 50;
 
 for (i = 1; i <= N; i++) {
-    if (i % 3 === 0) {
-        console.log(`O número divísel por 3 é ${i}`);
-    }
+  if (i % 3 === 0) {
+    console.log(`O número divísel por 3 é ${i}`);
+  }
 }
